cacdfd9daab88f70cb81760c1c7b8f1fa4561fcd
meh <meh@paranoici.org>
2011-01-22 21:17:47 +0100

    Neh.

--- lib/failirc/server/modules/base.rb
+++ lib/failirc/server/modules/base.rb
@@ -29,8 +29,6 @@ require 'failirc/responses'
 module IRC; class Server
 
 Module.define('base', '0.0.1') {
-  Incoming = IRC::Server::Incoming
-
   module Flags
     Groups = {
       :can_change_channel_modes => [
